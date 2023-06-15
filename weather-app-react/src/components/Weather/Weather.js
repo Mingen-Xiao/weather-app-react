a331571466e047406c05660a348dc9b46797b27f
Lucas Xiao <smy240975417@gmail.com>
2023-06-15 15:26:41 +0930

    Set up API from weather api

--- weather-app-react/src/components/Weather/Weather.js
+++ weather-app-react/src/components/Weather/Weather.js
@@ -1,6 +1,11 @@
 import './Weather.css';
 import {useState} from 'react';
 
+const API_KEY = 'e32ccd46634145978b655340231506';
+
+const URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=Beijing&aqi=no`;
+
+
 const Weather = () => {
     const [city, setCity] = useState('');
     const onCityInputChange = (event) => {
@@ -12,6 +17,8 @@ const Weather = () => {
         <div>
             <h1>JR Weather App 🌤</h1>
             <input type='text' placeholder='Search city...' value={city} onChange={onCityInputChange}/>
+            <button>Search</button>
+
         </div>
     );
 }
