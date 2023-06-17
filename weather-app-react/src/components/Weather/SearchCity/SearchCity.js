d1ae112f62ef818bd4d43e851b33fc831b24b9c7
Lucas Xiao <smy240975417@gmail.com>
2023-06-17 16:45:11 +0930

    apply Bootstrap to generate new bar and tick button

--- weather-app-react/src/components/Weather/SearchCity/SearchCity.js
+++ weather-app-react/src/components/Weather/SearchCity/SearchCity.js
@@ -1,6 +1,9 @@
 import './SearchCity.css';
 import { fetchWeatherByCity } from '../../../services/weatherService';
 import {useState} from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 
 const SearchCity = (props) => {
 
@@ -18,19 +21,38 @@ const SearchCity = (props) => {
     // function when click the search button
     // async...await：'promise'的一个语法
     // async：会把这个function return的所有东西放到'promise'里
-    const onSearchButtonClick = async () => {
+    const onSearchButtonClick = async (event) => {
+        event.preventDefault();
         const weatherData = await fetchWeatherByCity(city);
         // setWeather(weatherData);
         props.search(weatherData);
     }
         
     return (
-        <div>
-            {/* the value is 'city', with the onChange function when user input something */}
-            <input type='text' placeholder='Search city...' value={city} onChange={onCityInputChange}/>
-            {/* onClick: call function when clicked */}
-            <button onClick={onSearchButtonClick}>Search</button>
-        </div>
+        <>
+            <Form onSubmit={onSearchButtonClick}>
+                <Form.Group className="mb-3">
+                    <Form.Control
+                        type="text"
+                        placeholder="Search city.."
+                        value={city}
+                        onChange={onCityInputChange}
+                    />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Check type="checkbox" label="Show air quality data" className='air-quality'/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Search
+                </Button>
+            </Form>
+            {/* <div> */}
+                {/* the value is 'city', with the onChange function when user input something */}
+                {/* <input type='text' placeholder='Search city...' value={city} onChange={onCityInputChange}/> */}
+                {/* onClick: call function when clicked */}
+                {/* <button onClick={onSearchButtonClick}>Search</button> */}
+            {/* </div> */}
+        </>
     );
 }
 
--- weather-app-react/src/components/Weather/WeatherResult/WeatherResult.js
+++ weather-app-react/src/components/Weather/WeatherResult/WeatherResult.js
@@ -10,5 +10,4 @@ const WeatherResult = (props) => {
     );
 }
 
-
 export default WeatherResult;
